a4da7b5c77ce7804eb61556b765760ce9b0b8bae
elementorbot <elementorbot@users.noreply.github.com>
2024-01-17 15:29:15 +0000

    deploy: b1dd6b695697261378045e3193d657f82015d7cf

--- tests/jest/unit/modules/ai/assets/js/editor/mock/data.js
+++
@@ -1,5 +0,0 @@
-export const SCREENSHOT_LIGHT_1 = {
-	screenshot: 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mO8c/lyPQAIGAMDxstaTgAAAABJRU5ErkJggg==',
-	template: {},
-	baseTemplateId: '12345',
-};
